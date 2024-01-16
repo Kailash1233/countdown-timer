095fbd83b38f4d5ce766e13ff891d00c2ed9fb79
Kailash1233 <kailash61203@gmail.com>
2024-01-16 22:35:10 +0530

    first commit

---
+++ src/components/Particles.js
@@ -0,0 +1,8 @@
+import Particles from "@tsparticles/react";
+
+export default ParticlesComponent = () => {
+const options = useMemo(() => {
+    
+})    
+    return <Particles init={particlesInit} options={options} />
+}
